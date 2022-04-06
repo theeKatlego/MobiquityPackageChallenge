24d4161dff0e5aa09c0a41531fadb599b3de6f42
Katlego <theekatlego@gmail.com>
2022-04-06 20:46:31 +0200

    Add MediatR

---
+++ MobiquityPackageChallenge.Infrastructure/DependencyInjection/ApplicationContainerBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+using MediatR.Pipeline;
+using Microsoft.Extensions.DependencyInjection;
+using MobiquityPackageChallenge.Application.Packer;
+
+namespace MaxDrive.Infrastructure.DependencyInjection
+{
+    public static class ApplicationContainerBuilderExtensions
+    {
+        public static void AddApplication(this IServiceCollection services, Assembly applicationAssembly)
+        {
+            services.AddValidatorsFromAssembly(applicationAssembly);
+            services.AddMediatR(applicationAssembly);
+
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(RequestPreProcessorBehavior<,>));
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(RequestPostProcessorBehavior<,>));
+            AddApplicationServices(services);
+        }
+        private static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IExtractor, Extractor>();
+        }
+    }
+}
