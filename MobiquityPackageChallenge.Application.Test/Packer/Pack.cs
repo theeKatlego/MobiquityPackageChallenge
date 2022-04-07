c2e6988cd9c50eb024bc2b75e81d5cc1d0c7e8ef
Katlego <theekatlego@gmail.com>
2022-04-07 22:55:45 +0200

    add test project

---
+++ MobiquityPackageChallenge.Application.Test/Packer/Pack.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using MobiquityPackageChallenge.Application.Packer;
+using MobiquityPackageChallenge.Application.Test.Properties;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MobiquityPackageChallenge.Application.Test.Packer
+{
+    public class PackHandlerTest
+    {
+        private PackHandler _Handler;
+
+        public PackHandlerTest()
+        {
+            _Handler = new PackHandler(new Extractor());
+        }
+
+        [Fact]
+        public async Task Hadle_WhenCalled_ReturnCorrrect8Async()
+        {
+            // Arrange
+            var command = new PackCommand(Resources.example_input);
+
+            var expected = @"4-
+2,7
+8,9";
+
+            // Act
+            var actual = await _Handler.Handle(command, default);
+
+            // Assert
+            actual.Should().Be(expected);
+
+        }
+    }
+}
